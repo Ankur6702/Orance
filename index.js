5c1865e2b12e2bfa8576284769de982eff2307ee
Ankur Agarwal <ankuragarwa246@gmail.com>
2021-05-14 00:03:38 +0530

    Fixed images in homepage

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 let dropdown = document.getElementById("dropdownOnclick");
 let activeId = document.getElementById("active");
+let imageHover1 = document.getElementById("comp1");
+let imageHover2 = document.getElementById("comp2");
+let imageHover3 = document.getElementById("comp3");
 let c = 0;
 
 function drop() {
@@ -14,6 +17,29 @@ function main() {
 
     dropdown.addEventListener('click', function() {
         drop();
+    });
+
+    imageHover1.addEventListener("mouseover", function() {
+        document.querySelectorAll(".imageText")[0].style.visibility = "visible";
+    });
+    imageHover1.addEventListener("mouseout", function() {
+        document.querySelectorAll(".imageText")[0].style.visibility = "hidden";
+    });
+
+
+    imageHover2.addEventListener("mouseover", function() {
+        document.querySelectorAll(".imageText")[1].style.visibility = "visible";
+    });
+    imageHover2.addEventListener("mouseout", function() {
+        document.querySelectorAll(".imageText")[1].style.visibility = "hidden";
+    });
+
+
+    imageHover3.addEventListener("mouseover", function() {
+        document.querySelectorAll(".imageText")[2].style.visibility = "visible";
+    });
+    imageHover3.addEventListener("mouseout", function() {
+        document.querySelectorAll(".imageText")[2].style.visibility = "hidden";
     });
 
 }
