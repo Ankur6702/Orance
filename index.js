cac040a4a121d2edda9955405b3e288bee6f5a4c
Ankur Agarwal <ankuragarwa246@gmail.com>
2021-05-14 18:16:37 +0530

    modified class names in homepage

--- index.js
+++ index.js
@@ -1,47 +1,20 @@
-let dropdown = document.getElementById("dropdownOnclick");
-let activeId = document.getElementById("active");
-// let imageHover1 = document.getElementById("comp1");
-// let imageHover2 = document.getElementById("comp2");
-// let imageHover3 = document.getElementById("comp3");
+let dropdown = document.getElementById("homepage_dropdownOnclick");
+let activeId = document.getElementById("homepage_active");
+
 let c = 0;
 
 function drop() {
     if (c % 2 == 0)
-        activeId.classList.add('active');
+        activeId.classList.add('homepage_active');
     else
-        activeId.classList.remove('active');
+        activeId.classList.remove('homepage_active');
     c++;
 }
 
 function main() {
-
     dropdown.addEventListener('click', function() {
         drop();
     });
-
-    // imageHover1.addEventListener("mouseover", function() {
-    //     document.querySelectorAll(".imageText")[0].style.visibility = "visible";
-    // });
-    // imageHover1.addEventListener("mouseout", function() {
-    //     document.querySelectorAll(".imageText")[0].style.visibility = "hidden";
-    // });
-
-
-    // imageHover2.addEventListener("mouseover", function() {
-    //     document.querySelectorAll(".imageText")[1].style.visibility = "visible";
-    // });
-    // imageHover2.addEventListener("mouseout", function() {
-    //     document.querySelectorAll(".imageText")[1].style.visibility = "hidden";
-    // });
-
-
-    // imageHover3.addEventListener("mouseover", function() {
-    //     document.querySelectorAll(".imageText")[2].style.visibility = "visible";
-    // });
-    // imageHover3.addEventListener("mouseout", function() {
-    //     document.querySelectorAll(".imageText")[2].style.visibility = "hidden";
-    // });
-
 }
 
 main();
